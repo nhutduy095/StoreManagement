b6afa3c7febb30f2051fa1389d17851cbfed8473
DuyNVN <duynhut0095@gmail.com>
2024-05-12 21:08:46 +0700

    commit view

---
+++ StoreManagement/StoreManagement/Controllers/ShopController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreManagement.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ShopDetail()
+        {
+            return View();
+        }
+    }
+}
